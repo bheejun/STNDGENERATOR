e4b100cf9ecb00153f46fb53005ae09cf22c0cd7
hjbyeon <bhjun95@gmail.com>
2026-07-15 16:48:31 +0900

    chore: set default server port to 38081

--- src/test/java/kr/wise/csr/CommonStandardRuleBuilderApplicationTests.java
+++ src/test/java/kr/wise/csr/CommonStandardRuleBuilderApplicationTests.java
@@ -1,13 +1,24 @@
 package kr.wise.csr;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class CommonStandardRuleBuilderApplicationTests {
+	@Autowired
+	Environment environment;
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void usesTheWdqLocalServerPortByDefault() {
+		assertThat(environment.getProperty("server.port", Integer.class)).isEqualTo(38081);
 	}
 
 }
