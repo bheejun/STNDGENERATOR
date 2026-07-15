0485b91c452a364a2f3ecc4479c852bf81c5df87
hjbyeon <bhjun95@gmail.com>
2026-07-15 16:37:11 +0900

    chore: initialize Spring Boot project

---
+++ src/main/java/kr/wise/csr/CommonStandardRuleBuilderApplication.java
@@ -0,0 +1,13 @@
+package kr.wise.csr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonStandardRuleBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonStandardRuleBuilderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/wise/csr/CommonStandardRuleBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.wise.csr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonStandardRuleBuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
